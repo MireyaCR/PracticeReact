5df463fc16c85d546bbb678e1029be929a0fa38f
MireyaCR <mireya.mcr@gmail.com>
2022-10-10 19:54:05 +0000

    react1

--- exercises/01.3-building-a-layout/app.jsx
+++ exercises/01.3-building-a-layout/app.jsx
@@ -12,8 +12,22 @@ const data = {
 	},
 };
 
-let content = (
-		<img src={data.image} />
+const content = (
+	<div className="card m-5">
+		<img className="card-img-top" src={data.image} alt="Card image cap" />
+		<div className="card-body">
+			<h5 className="card-title">{data.cardTitle}</h5>
+			<p className="card-text">{data.cardDescription}</p>
+			<a href={data.button.url} className="btn btn-primary">
+				{data.button.label}
+			</a>
+		</div>
+	</div>
+	// >
+	// <img class="card-img-top" src={data.image} alt="Card image cap" />
+	// >
+
+	//
 );
 /**
  * define the variable 'content' here and fill it with the
