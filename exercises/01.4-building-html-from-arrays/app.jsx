f10fab8619582c709af240da78387801362acf3c
MireyaCR <mireya.mcr@gmail.com>
2022-10-12 15:12:02 +0000

    react1

--- exercises/01.4-building-html-from-arrays/app.jsx
+++ exercises/01.4-building-html-from-arrays/app.jsx
@@ -2,12 +2,10 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [
-    <ul className="nav">
-        <li className="nav-item"><a className="nav-link" href="#">Link to google.com</a></li>
-        <li className="nav-item"><a className="nav-link" href="#">Link to facebook.com</a></li>
-        <li className="nav-item"><a class="nav-link" href="#">Link to amazon.com</a></li>
-    </ul>
+const navlinkItems = [	
+		<li className="nav-item"><a className="nav-link" href="#">Link to google.com</a></li>,
+		<li className="nav-item"><a className="nav-link" href="#">Link to facebook.com</a></li>,
+		<li className="nav-item"><a className="nav-link" href="#">Link to amazon.com</a></li>
 ];
 
 const content = <ul className="nav">{navlinkItems}</ul>;
